b16f2dec919784a29fc05e8b0ab314698eb4fc20
elvisgraho <elvis-gra@pm.me>
2026-01-20 11:50:55 +0100

    Bugfixes

--- src/ui/components/logs.py
+++ src/ui/components/logs.py
@@ -13,7 +13,7 @@ def render_logs_tab():
     line_count = st.selectbox(
         "Lines",
         options=[50, 100, 200],
-        index=200,
+        index=2,
         key="log_lines_select"
     )
     
